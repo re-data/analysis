09e161c654616a68c1e81794618a7e2cc44850ae
Mateusz Klimek <mateusz@getre.io>
2022-05-19 18:04:34 +0000

    update

---
+++ models/marts/by_actions.sql
@@ -0,0 +1,17 @@
+select
+    user_id,
+    os_system,
+    dbt_version,
+    re_data_version,
+    to_date(timestamp) as date,
+    array_agg(distinct command) as commands,
+    min(timestamp) as min_timestamp,
+    max(timestamp) as max_timestamp,
+    count(*)
+from {{ ref('calls')}}
+where
+    timestamp > '2022-05-06' :: timestamp and
+    (status = 'success' or command = 'serve')
+group by
+    user_id, os_system, dbt_version, re_data_version, to_date(timestamp)
+order by to_date(timestamp) desc;
