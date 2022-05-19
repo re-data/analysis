29f13bc308ff84c2d12997687878f0cde1858603
Mateusz Klimek <mateusz@getre.io>
2022-05-19 18:31:40 +0000

    update

--- models/marts/by_actions.sql
+++ models/marts/by_actions.sql
@@ -8,11 +8,11 @@ select
     array_agg(distinct command) as commands,
     min(timestamp) as min_timestamp,
     max(timestamp) as max_timestamp,
-    count(*)
+    count(*) as count
 from {{ ref('calls')}}
 where
     timestamp > '2022-05-06' :: timestamp and
     (status = 'success' or command = 'serve')
 group by
     user_id, os_system, dbt_version, re_data_version, python_version, to_date(timestamp)
-order by to_date(timestamp) desc;
+order by to_date(timestamp) desc
