d52bb3413ca4940012e74be0b17276ac8dc2d7b8
Mateusz Klimek <mateusz@getre.io>
2022-05-19 18:08:18 +0000

    update

--- models/marts/by_actions.sql
+++ models/marts/by_actions.sql
@@ -3,6 +3,7 @@ select
     os_system,
     dbt_version,
     re_data_version,
+    python_version,
     to_date(timestamp) as date,
     array_agg(distinct command) as commands,
     min(timestamp) as min_timestamp,
@@ -13,5 +14,5 @@ where
     timestamp > '2022-05-06' :: timestamp and
     (status = 'success' or command = 'serve')
 group by
-    user_id, os_system, dbt_version, re_data_version, to_date(timestamp)
+    user_id, os_system, dbt_version, re_data_version, python_version, to_date(timestamp)
 order by to_date(timestamp) desc;
