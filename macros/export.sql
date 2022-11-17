6b14d376a35bbb5cce74d1b12d3ee789decce75e
Mateusz Klimek <mateusz@getre.io>
2022-11-17 22:49:34 +0100

    update

--- macros/export.sql
+++ macros/export.sql
@@ -1,7 +1,7 @@
 
 {% macro export_exceptions() %}
     {% set query %}
-        select * from {{ ref('exceptions_by_day')}} where library = 're_data'
+        select * from {{ ref('exceptions_by_day')}}
     {% endset %}
     {% set result = run_query(query) %}
     {% do result.to_json('views/exceptions.json') %}
