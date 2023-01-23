8174147d94f77f9be6dd157f6a6dfff9ad639f89
Mateusz Klimek <mateusz@getre.io>
2023-01-23 15:08:08 +0000

    Remove not used files

--- views/exceptions.py
+++
@@ -1,7 +0,0 @@
-import plotly.express as px
-import pandas as pd
-
-df = pd.read_json("exceptions.json")
-fig = px.line(df, x="DATE", y="DISTINCT_USERS", title='Daily user exceptions')
-
-fig.write_html("index.html")
